d321255d2efd0d1151bb35ae017d6eda1c121783
brianzammit <brian.zammit.09@gmail.com>
2014-04-07 14:10:28 +0200

    JUnit 1

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+
+/**
+ *
+ * @author Christopher
+ */
 public class Calculator {
-	
-public static void main (String args[])
+    
+   public int add(int a, int b)
 {
-	System.out.println("Hello Calculator!");
+    return a+b;
+}
+
+public int subtract(int a, int b)
+{
+    return a-b;
+}
+
+public int multiply(int a, int b)
+{
+    return a*b;
+}
+
+public int divide(int a, int b)
+{
+    if(b == 0){
+        return -999;}
+    else{
+        return a/b;}
+}
+
+public String getMessage(int n)
+{
+    String s = "";
+    for(int i = 0; i < n; i++){
+        s = s +  "Hello World!\n";}
+    
+    return s;
+}
+
+public int addString(String numbers)
+{
+    return 0;
 }
 }
