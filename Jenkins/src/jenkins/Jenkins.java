5cdb7aa5e968ab066fc4f6d29ee07d3f8351242e
Jes31 <jesbut31@gmail.com>
2014-04-07 14:30:54 +0200

    added

---
+++ Jenkins/src/jenkins/Jenkins.java
@@ -0,0 +1,30 @@
+package jenkins;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Jenkins {
+    /**
+     * @param args the command line arguments
+     */
+    
+    @Test
+    public static void TestCalc()
+    {
+        Calculator calc = new Calculator();
+        //assertEquals("4/0 = ", -999, calc.divide(4,0));
+        assertEquals("Hello World Jessica!\n", calc.getMessage(1));
+        //calc.divide(4,0 World);
+    }
+    
+    //@Test
+    //public static void 
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*Calculator calc = new Calculator();
+        
+        System.out.println(calc.divide(4,0));*/
+        TestCalc();
+    }
+}
