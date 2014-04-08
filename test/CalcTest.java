1229764415e76f2b06263cde9b58182cb3b735dc
Jes31 <jesbut31@gmail.com>
2014-04-08 12:52:55 +0200

    sets

--- test/CalcTest.java
+++ test/CalcTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import jenkins.Calculator;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -11,10 +6,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Christopher
- */
+
 public class CalcTest {
     
     public CalcTest() {
